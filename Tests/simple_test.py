6f6386718dd5bf9ab8fb27b71b23ab957722e565
PavlovEgor <pavlov.ep@phystech.edu>
2026-04-29 13:35:35 +0300

    add incompressible corrector

--- Tests/simple_test.py
+++ Tests/simple_test.py
@@ -9,6 +9,7 @@ def simple_test(flow, particles, model):
 
     particles.evolve(flow, dt)
     model.predict(particles)
+    model.correct()
     err = model.error(flow, 1)
 
     print("Error of model = ", err)
